5e972fe2152a018297f92d462dd3f8e0b4de5cd0
rmznsn <32266249+rmznsn@users.noreply.github.com>
2019-05-04 16:02:27 +0300

    First commit

---
+++ BlockChain/Blockchain/Block.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blockchain
+{
+   public class Block
+    {
+        private DateTime Times { get; set; }
+        private int ProductID { get; set; }
+        private int CompanyID { get; set; }
+        private string Data { get; set; }
+        private string Hash { get; set; }
+        private string PreviousHash { get; set; }
+        private int Index { get; set; }
+
+
+        public Block(DateTime times, string data, string previoushash, int index, int companyID, int productID)
+        {
+            Times = times;
+            Data = data;
+            Hash = calculateHash();
+            PreviousHash = previoushash;
+            Index = index;
+            CompanyID = companyID;
+            ProductID = productID;
+        }
+
+        public string calculateHash()
+        {
+            SHA256 sHA256 = SHA256.Create();
+            byte[] input = Encoding.ASCII.GetBytes(Data + ProductID.ToString() + CompanyID.ToString() + DateTime.Now);
+            byte[] output = sHA256.ComputeHash(input);
+
+            return Convert.ToString(output);
+
+
+        }
+    }
+}
---
+++ BlockChain/Blockchain/BlockChain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blockchain
+{
+  public  class BlockChain
+    {
+        public IList<Block> chain { get; set; }
+
+        public BlockChain()
+        {
+            InitializeChain();
+        }
+        public void InitializeChain()
+        {
+            chain = new List<Block>();
+        }
+
+        public Block CreateBlock(string data, string previoushash, int index, int productID, int companyID)
+        {
+            return new Block(DateTime.Now, data, previoushash, index, productID, companyID);
+        }
+
+    }
+}
